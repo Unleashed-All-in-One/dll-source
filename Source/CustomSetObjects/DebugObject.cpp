e6a18499e1117672b2f07c40ef089a0d24a996a6
Lorenzo Raineri <lorenzorai05@gmail.com>
2024-02-05 16:24:32 +0100

    Mostly working EvilSonic Pole
    PLEASE rewrite this!!!

--- Source/CustomSetObjects/DebugObject.cpp
+++ Source/CustomSetObjects/DebugObject.cpp
@@ -1,9 +1,95 @@
+namespace Sonic::Player
+{
 
+    class IPosture : public Hedgehog::Universe::CStateMachineBase::CStateBase
+    {
+    public:
+        Hedgehog::Base::CSharedString m_ClassName;
+
+        IPosture(const bb_null_ctor&) : CStateBase(bb_null_ctor{}) {}
+        IPosture(const Hedgehog::Base::CSharedString& name) : m_ClassName(name) {}
+        //IPosture() : IPosture(bb_null_ctor{}) {}
+    };
+
+    class CPlayerSpeedPostureCommon : public IPosture
+    {
+    public:
+        float m_Unk1 = 0.0f;
+        float m_Unk2 = 0.0f;
+        float m_Unk3 = 0.0f;
+        Hedgehog::Math::CVector m_Vector = Hedgehog::Math::CVector(0, 0, 0);
+
+        CPlayerSpeedPostureCommon(const bb_null_ctor&) : IPosture(bb_null_ctor{}) {}
+        //CPlayerSpeedPostureCommon() : CPlayerSpeedPostureCommon(bb_null_ctor{})
+
+        CPlayerSpeedPostureCommon() : IPosture("CPlayerSpeedPostureCommon")
+        {
+            *(int*)this = 0x016D3B6C;
+        }
+
+        CPlayerSpeedPostureCommon(const Hedgehog::Base::CSharedString& name) : IPosture(name)
+        {
+            *(int*)this = 0x016D3B6C;
+        }
+    };
+
+    class CPlayerSpeedPosture3DCommon : public CPlayerSpeedPostureCommon
+    {
+    public:
+        CPlayerSpeedPosture3DCommon(const bb_null_ctor&) : CPlayerSpeedPostureCommon(bb_null_ctor{}) {}
+
+        CPlayerSpeedPosture3DCommon() : CPlayerSpeedPostureCommon("CPlayerSpeedPosture3DCommon")
+        {
+            *(int*)this = 0x016D392C;
+        }
+
+        CPlayerSpeedPosture3DCommon(const Hedgehog::Base::CSharedString& name) : CPlayerSpeedPostureCommon(name)
+        {
+            *(int*)this = 0x016D392C;
+        }
+
+        void UpdateState() override
+        {
+            BB_FUNCTION_PTR(void, __thiscall, MovementRoutine, 0x00E37FD0, void* This);
+            MovementRoutine(this);
+        }
+
+    };
+}
 // The other macros are your object's class name, not XML name.
 BB_SET_OBJECT_MAKE_HOOK(WerehogPole);
+class OnPolePosture : public Sonic::Player::CPlayerSpeedPosture3DCommon
+{
+public:
+	static constexpr const char* ms_pStateName = "PoleGrip";
+
+	
+	Sonic::Player::CPlayerSpeedContext* GetContext() const
+	{
+		return static_cast<Sonic::Player::CPlayerSpeedContext*>(m_pContext);
+	}
+	void EnterState() override
+	{
+	}
+	void UpdateState() override
+	{
+		
+	}
+	virtual void UnknownStateFunction(void* a1) {}
+};
+
+HOOK(void*, __fastcall, _InitializePlayer, 0x00D96110, void* This)
+{
+	void* result = original_InitializePlayer(This);
+	auto context = Sonic::Player::CPlayerSpeedContext::GetInstance();    // Hack: there's a better way to do this but whatever. This writes to the singleton anyway.
+
+	context->m_pPlayer->m_PostureStateMachine.RegisterStateFactory<OnPolePosture>();
+	return result;
+}
 
 // Whatever your entrypoint is, you'll need this:
 void WerehogPole::Install()
 {
+	INSTALL_HOOK(_InitializePlayer);
     BB_INSTALL_SET_OBJECT_MAKE_HOOK(WerehogPole)
 }
--- Source/CustomSetObjects/DebugObject.h
+++ Source/CustomSetObjects/DebugObject.h
@@ -1,5 +1,5 @@
 #pragma once
-
+using namespace hh::math;
 static const int* pColID_Common = reinterpret_cast<int*>(0x01E0AF30);
 class ConversionUtility
 {
@@ -27,8 +27,30 @@ public:
         y = ny + py;
         player.x() = x;
         player.z() = y;
-        return      Hedgehog::Math::CVector(x, player.y(), y);
+        return Hedgehog::Math::CVector(x, player.y(), y);
     }
+    static CQuaternion QuaternionFromAngleAxisUtil(float angle, const CVector& axis)
+    {
+        CQuaternion q;
+        float m = sqrt(axis.x() * axis.x() + axis.y() * axis.y() + axis.z() * axis.z());
+        float s = sinf(angle / 2) / m;
+        q.x() = axis.x() * s;
+        q.y() = axis.y() * s;
+        q.z() = axis.z() * s;
+        q.w() = cosf(angle / 2);
+        return q;
+    }
+
+    static CQuaternion QuaternionFaceTowards(const Eigen::Vector3f& targetPoint, const Eigen::Vector3f& position, CQuaternion rotation) {
+        Eigen::Vector3f currentForward = rotation * Eigen::Vector3f::UnitZ();
+        Eigen::Vector3f targetDirection = (targetPoint - position).normalized();
+
+        Eigen::Quaternionf rotationQuaternion;
+        rotationQuaternion.setFromTwoVectors(currentForward, targetDirection);
+
+        return rotation = rotationQuaternion * rotation;
+    }
+   
 };
 static const int* pColID_PlayerEvent = reinterpret_cast<int*>(0x01E0AFD8);
 class WerehogPole :public Sonic::CObjectBase, public Sonic::CSetObjectListener
@@ -47,7 +69,7 @@ public:
     bool SetAddRenderables(Sonic::CGameDocument* in_pGameDocument, const boost::shared_ptr<Hedgehog::Database::CDatabase>& in_spDatabase) override
     {
         /* returning false if your renderable doesn't exist is recommended, your object simply wont render / will cease construction or w/e. */
-        const char* assetName = "chr_Sonic_HD";
+        const char* assetName = "evl_myk_obj_soc_dialopleAaMid5m_000";
         hh::mr::CMirageDatabaseWrapper wrapper(in_spDatabase.get());
         boost::shared_ptr<hh::mr::CModelData> spModelData = wrapper.GetModelData(assetName, 0);
         m_spExampleElement = boost::make_shared<hh::mr::CSingleElement>(spModelData);
@@ -109,6 +131,7 @@ public:
     bool inPole;
     float add;
     float add2;
+    float add3;
     float cooldownTimer;
     float side = 0;
     void AddImpulse(Hedgehog::Math::CVector impulse, bool relative)
@@ -125,6 +148,23 @@ public:
             return;
         }
     }
+    void rotateQuaternionTowardsVector(Eigen::Quaternionf& quat, const Eigen::Vector3f& targetVector) {
+        Eigen::Vector3f currentVector = quat * Eigen::Vector3f::UnitZ();
+        Eigen::Quaternionf rotation;
+
+        rotation.setFromTwoVectors(currentVector, targetVector);
+
+        quat = rotation * quat;
+    }
+    CQuaternion faceTowards(const Eigen::Vector3f& targetPoint, const Eigen::Vector3f& position, CQuaternion rotation) {
+        Eigen::Vector3f currentForward = rotation * Eigen::Vector3f::UnitZ();
+        Eigen::Vector3f targetDirection = (targetPoint - position).normalized();
+
+        Eigen::Quaternionf rotationQuaternion;
+        rotationQuaternion.setFromTwoVectors(currentForward, targetDirection);
+
+        return rotation = rotationQuaternion * rotation;
+    }
     // You'll do update logic here for the most part.
     // Again don't add this if you don't need to, but you usually will for interactables.
     void SetUpdateParallel(const hh::fnd::SUpdateInfo& in_rUpdateInfo) override
@@ -137,20 +177,46 @@ public:
         {
             if (inputPtr->IsTapped(Sonic::eKeyState_A))
             {
-                auto quat = Hedgehog::math::CQuaternion(1, -playerContext->m_spMatrixNode->m_Transform.m_Rotation.x(), playerContext->m_spMatrixNode->m_Transform.m_Rotation.y(), -playerContext->m_spMatrixNode->m_Transform.m_Rotation.z());
-                AddImpulse(quat * Hedgehog::Math::CVector(100, 100, 100), true);
+                playerContext->m_Velocity = CVector(0, 0, 0);
+                AddImpulse(playerContext->m_spMatrixNode->m_Transform.m_Rotation * Hedgehog::Math::CVector(0, 5, -130), true);
+                playerContext->ChangeState(Sonic::Player::StateAction::Jump);
+                playerContext->SetStateFlag(Sonic::Player::CPlayerSpeedContext::EStateFlag::eStateFlag_IgnorePadInput, 1);
+
+                playerContext->m_pPlayer->m_PostureStateMachine.ChangeState("Standard");
                 inPole = false;
             }
-            add += inputPtr->LeftStickVertical / 10;
+            add += inputPtr->LeftStickVertical / 30;
+            add2 += inputPtr->RightStickVertical;
+
+            m_spMatrixNodeTransform->m_Transform.m_Rotation = ConversionUtility::QuaternionFromAngleAxisUtil(add2, CVector(0, 1, 0));
+            add3 += inputPtr->RightStickHorizontal / 10;
             if (inputPtr->IsTapped(Sonic::eKeyState_DpadRight))
             {
                 side++;
-                add2 = 90 * side;
+                add3 += 1.5f;
             }
-            Common::ClampFloat(add, 0, 10);
-            playerContext->m_spMatrixNode->m_Transform.m_Position = Hedgehog::Math::CVector(m_spMatrixNodeTransform->m_Transform.m_Position.x() + 1, m_spMatrixNodeTransform->m_Transform.m_Position.y() + add, m_spMatrixNodeTransform->m_Transform.m_Position.z());
-            playerContext->m_spMatrixNode->m_Transform.m_Position = ConversionUtility::MoveAroundPivot(playerContext->m_spMatrixNode->m_Transform.m_Position, m_spMatrixNodeTransform->m_Transform.m_Position, Hedgehog::math::CVector(add2, 0, 0));
+            if (add3 > 4.5f)
+                add3 = 0;
+            if (add3 < 0)
+                add3 = 4.5f;
+            playerContext->ChangeAnimation("Evilsonic_pillar_idle");
+            playerContext->SetStateFlag(Sonic::Player::CPlayerSpeedContext::EStateFlag::eStateFlag_IgnorePadInput, 1 );
+            auto playerPos = playerContext->m_spMatrixNode->m_Transform.m_Position;
+            auto targetPos = m_spMatrixNodeTransform->m_Transform.m_Position;
 
+           targetPos.y() = playerPos.y();
+            CVector veca = targetPos - playerPos;
+            CVector vecb = CVector(1, 0, 1); // this is always the direction the particle is initially created in.
+            veca.normalize();
+            CVector axis = veca.cross(vecb);
+            axis.normalize();
+            DebugDrawText::log(std::format("Add3: {0} | Side: {1}", add3, side).c_str(), 0);
+            playerContext->m_spMatrixNode->m_Transform.m_Rotation = ConversionUtility::QuaternionFaceTowards(targetPos, playerPos, playerContext->m_spMatrixNode->m_Transform.m_Rotation);
+            //rotateQuaternionTowardsVector(playerContext->m_spMatrixNode->m_Transform.m_Rotation, m_spMatrixNodeTransform->m_Transform.m_Position);
+            playerContext->m_spMatrixNode->m_Transform.m_Position = Hedgehog::Math::CVector(m_spMatrixNodeTransform->m_Transform.m_Position.x() + 1, m_spMatrixNodeTransform->m_Transform.m_Position.y() + add, m_spMatrixNodeTransform->m_Transform.m_Position.z());
+            playerContext->m_spMatrixNode->m_Transform.m_Position = ConversionUtility::MoveAroundPivot(playerContext->m_spMatrixNode->m_Transform.m_Position, m_spMatrixNodeTransform->m_Transform.m_Position, Hedgehog::math::CVector(add3, 0, add3));
+
+            
         }
     }
 
